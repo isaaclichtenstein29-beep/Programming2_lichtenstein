c4c4b010576aba25ca16d29bcfa09d04fb82ae5f
isaaclichtenstein29-beep <isaac.lichtenstein29@paceacademy.org>
2026-01-16 09:22:18 -0500

    List

---
+++ game.py
@@ -0,0 +1,49 @@
+#Create Programming2.py
+##An adventure game similar to The Oregon Trail game, in which a main character 
+##and his group must travel across the country in the middle of a zombie apocalypse. 
+##The characters are attacked by various kinds of zombies, some fast, strong, flying, 
+##or large. The character must keep good storage of his food, and resist being killed 
+##between checkpoints, which are very far away. During the journey, the character can be
+##bitten, as well as his crew, and must be left behind. If a person is bitten they will
+##die. However, to prevent death, an antidote can be found however they are rare, and
+##difficult to obtain.
+
+
+#global variables / point values
+
+
+#Would you like to begin?
+
+#supplies
+Money= chr(128184)
+supplies= {(Money, 1000), ("Food", 0), ("Horse", 2),("Antidotes", 1)}
+print(supplies)
+#What are the names of your characters?
+name1= input("What is the name of your main character")
+name2= input("What is the name of your second character")
+name3= input("What is the name of your third character")
+name4= input("What is the name of your fourth character")
+name5= input("What is the name of your fifth character")
+#open shop
+
+#supplies
+
+#Begin Journey
+
+#Zombie encounters (random)
+
+#   character death (after an unantidoted bite)
+
+#loot find
+
+#next location
+
+#next location
+
+#next location
+
+#next location
+
+#end game
+
+#scoring
